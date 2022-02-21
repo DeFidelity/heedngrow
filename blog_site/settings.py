1fccbab0a9fa88c3a09a024c0b060bf24ca9f3ef
DeFidelity <adeyemihaki@gmail.com>
2022-02-21 10:38:56 +0100

    backend cleanup

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_admin_tailwind',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
